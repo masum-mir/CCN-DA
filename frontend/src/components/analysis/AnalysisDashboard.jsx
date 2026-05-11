6c1d141d776ddd97f88b88b23a95e491883236d5
masum <masummir773@gmail.com>
2026-05-11 07:15:40 +0600

    bug fixed analysis dashbard

--- frontend/src/components/analysis/AnalysisDashboard.jsx
+++ frontend/src/components/analysis/AnalysisDashboard.jsx
@@ -1,6 +1,7 @@
 import { useState, useRef, useEffect } from "react";
 import "../../styles/AnalysisDashboard.css";
-import { API } from "../../api/simulator.js";
+
+const API = import.meta.env.VITE_API_URL || "http://localhost:4000/api";
  
 // Plot card
 function PlotCard({ path, name }) {
